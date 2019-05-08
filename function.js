400d95a8882395a2059c524dab2f3bb5fb514847
Simone Lever <simonelever@gmail.com>
2019-05-08 12:39:02 +0200

    it can take itemSelector with role

--- function.js
+++ function.js
@@ -10,11 +10,6 @@ async function compFunc(page, structure, component, tag) {
         //prendo le resource per ogni nodo
         resources[i] = await page.evaluate((obj) => { return obj.getAttribute('bot-resource'); }, node[i]);
 
-        //prendo classe e id dal nodo per usarli per identificarlo
-        let clas = await page.evaluate((obj) => { return obj.getAttribute('class'); }, node[i]);
-        let id = await page.evaluate((obj) => { return obj.id; }, node[i]);
-        let tagName = await page.evaluate((obj) => { return obj.tagName; }, node[i]);
-
         //prendo i nodi con bot-attr e prendo gli attributes per ogni nodo, controllo che un determinato attributes non sia già stato letto
         attrNode = await node[i].$$('[bot-attribute]');
         let cont = 0;
@@ -30,19 +25,55 @@ async function compFunc(page, structure, component, tag) {
             }
             //se è true non è presente negli attributes già trovati, inserisco
             if (cont == attributes.length) {
-                attributes.push({name: temp, selector: '[bot-attribute='+temp+']'})
+                attributes.push({ name: temp, selector: '[bot-attribute=' + temp + ']' })
             }
         }
 
-        //inserisco la classe, id, NomeTag per riconoscere meglio il nodo
-        let tagToUse = tagName;
+        let containerSelector = await takeSelector(node[i], tag, page);
 
-        if(tag == '[role=\'list\']') {  tagToUse += tag;    }
-        if(id) {  tagToUse += "#"+id;    }
-        if(clas) {  tagToUse += "."+clas;    }
+        //inserisco item in selector
+        let itemSelector = [];
+        if (tag == 'ul' || tag == 'ol') {
+            itemSelector.push('LI')
+        } else {
+            let itemNodes = await node[i].$$('[role="listitem"]');
 
-        structure.push({ component: component, resource: resources[i], attributes: attributes, selector: tagToUse });
+            //ciclo sui nodi per prendere i vari tipi di tag contenenti item che ci possono essere
+            for (let i = 0; i < itemNodes.length; i++) {
+                let itemSelectorTemp = await takeSelector(itemNodes[i], null, page);
+                //controllo che non sia già presente
+                let contItem = 0;
+                for (let j = 0; j < itemSelector.length; j++) {
+                    if (itemSelectorTemp != itemSelector[j]) {
+                        contItem++;
+                    }
+                }
+                //se è true non è presente nei selector degli item già trovati, inserisco
+                if (contItem == itemSelector.length) {
+                    itemSelector.push(itemSelectorTemp);
+                }
+            }
+
+        }
+
+        structure.push({ component: component, resource: resources[i], attributes: attributes, selector: { container: containerSelector, item: itemSelector } });
     }
+}
+
+async function takeSelector(node, tag, page) {
+    //prendo classe, id e tag dal nodo per usarli per identificarlo
+    let clas = await page.evaluate((obj) => { return obj.getAttribute('class'); }, node);
+    let id = await page.evaluate((obj) => { return obj.id; }, node);
+    let tagName = await page.evaluate((obj) => { return obj.tagName; }, node);
+
+    //inserisco la classe, id, NomeTag per riconoscere meglio il nodo
+    let tagToUse = tagName;
+
+    if (tag == '[role=\'list\']') { tagToUse += tag; }
+    if (id) { tagToUse += "#" + id; }
+    if (clas) { tagToUse += "." + clas; }
+
+    return tagToUse;
 }
 
 module.exports = { compFunc };
