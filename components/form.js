3c0871887036e64fe4b3ea69565722666d26960d
Simone Lever <simonelever@gmail.com>
2019-08-22 16:06:38 +0200

    added base structure for form

---
+++ components/form.js
@@ -0,0 +1,22 @@
+async function form(page, structure, component, tag, node) {
+    //per gestire più form uso un for
+    for (let i = 0; i < node.length; i++) {
+        //estraggo la resource del form, ad esempio datiUtente
+        resources[i] = await page.evaluate((obj) => { return obj.getAttribute('bot-resource'); }, node[i]);
+
+        //prendo i nodi con bot-attr e prendo gli attributes per ogni nodo,
+        attrNode = await node[i].$$('[bot-attribute]');
+
+        for (let j = 0; j < attrNode.length; j++) {
+            //prendo il valore del bot-attr per questo nodo
+            temp = await page.evaluate((obj) => { return obj.getAttribute('bot-attribute'); }, attrNode[j]);
+            //se è true non è presente negli attributes già trovati, inserisco
+            attributes.push({ name: temp, selector: '[bot-attribute=' + temp + ']' })
+        }
+
+    }
+    
+    structure.push({ component: component });
+}
+
+module.exports = { form };
--- function.js
+++ function.js
@@ -1,13 +1,13 @@
 const LIST = require("./components/list.js");
+const FORM = require("./components/form.js");
 
 async function compFunc(page, structure, component, tag) {
     let node = await page.$$(tag);
     if(component == "list") {
         await LIST.list(page, structure, component, tag, node);
     } else if (component == "form") {
-        console.log("")
+        await FORM.form(page, structure, component, tag, node);
     }
-    console.log(structure);
 }
 
 module.exports = { compFunc };
